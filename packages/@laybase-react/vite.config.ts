df1c4e1a36d4e6be4c862d01506811a11c305a76
Kevin <kevin@KevindeMacBook-Pro.local>
2023-05-19 16:27:21 +0800

    :art: button

--- packages/@laybase-react/vite.config.ts
+++ packages/@laybase-react/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-// import libCss from 'vite-plugin-libcss'
 import unoCSS from 'unocss/vite'
+
+// import libCss from 'vite-plugin-libcss'
 
 export default defineConfig({
   build: {
