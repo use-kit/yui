ded7c20d686520f650240880bb8607d38e2b0985
Akashi <akashi_sai@163.com>
2023-05-25 08:32:07 +0800

    :construction: feat(shared): scan file

--- packages/@laybase-shared/src/utils/svg.ts
+++ packages/@laybase-shared/src/utils/svg.ts
@@ -1,9 +1,11 @@
-import fs from 'fs-extra'
+// import fs from 'fs-extra'
+import fs from 'node:fs'
+import fg from 'fast-glob'
 
 const iconPath = 'node_modules/@iconify-json'
 let icons: any = []
 
-function scan(prefix: string) {
+function read(prefix: string) {
   fs.readdirSync(iconPath, { withFileTypes: true }).forEach((lib) => {
     if (lib.name === prefix) {
       const json = fs.readFileSync(`${lib.path}/${prefix}/icons.json`, { encoding: 'utf-8' })
@@ -12,12 +14,25 @@ function scan(prefix: string) {
   })
 }
 
-export function getSvgBody(icon: string) {
+async function scan(perfix: string) {
+  const files = await fg([`${iconPath}/**/icons.json`])
+
+  for (const file of files) {
+    const lib = file.split('/').reverse()[1]
+
+    if (lib === perfix) {
+      const content = fs.readFileSync(file, 'utf-8')
+      icons = JSON.parse(content).icons
+    }
+  }
+}
+
+export async function getSvgBody(icon: string) {
   const [prefix, svg] = icon.split(':')
 
-  scan(prefix)
+  await scan(prefix)
   return icons[svg]
 }
 
-// const ret = getSvgBody('carbon:home')
+// const ret = await getSvgBody('carbon:home')
 // console.log('ret', ret)
